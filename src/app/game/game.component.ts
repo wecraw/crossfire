a209e9cd02f4ddfe87fac61787b9c070196d8ef1
William Crawford <william.crawford6@t-mobile.com>
2022-05-08 14:19:12 -0700

    cookies

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -82,7 +82,10 @@ export class GameComponent implements OnInit {
   invalidReason: string = ""; //reason for why a guess is invalid, shown in toast text
   toastTimeout: any;
 
-  practiceMode: boolean = false;
+  practiceMode: boolean = false; //set true for debugging
+  currentDay: number = 0; //days since epoch
+  hasWon: boolean = false; //true if user has won already in the daily
+  hasLost: boolean = false; //true if user has lost already in the daily
 
   showResetModal: boolean = false;
   showLossModal: boolean = false;
@@ -101,14 +104,43 @@ export class GameComponent implements OnInit {
 
   ngOnInit(): void {
     this.setClueSeeds()
-    // this.setDailySeed();
-    console.log(this.clueSeeds)
+    this.currentDay = this.daysSinceEpoch()
+    if (!this.isNewDay() && !this.practiceMode){
+      this.loadFromLocalStorage()
+
+      let el = document.getElementById("letters-row-wrapper")
+      setTimeout(function(){
+        el?.scrollTo({
+          top: el.scrollHeight,
+          behavior: 'smooth'
+        })
+      }, 1)
+
+    }
+    
+    //resetting win condition depends on clue not being set yet
+    if (this.hasWon){
+      this.currentLevel = 6
+      this.currentDisplayLevel = 6
+      this.entryIndex = -1;
+      this.handleWin()
+    }
+
     this.setClue()
+
+    //resetting loss condition depends on clue already being set
+    if (this.hasLost){
+      this.entryIndex = -1
+      this.handleLoss()
+    }
+
     this.gameWindowHeight = window.innerHeight - 265
   }
 
   reset(){
+    this.resetLocalStorage()
     this.submissions = []
+    this.enteredLetters = []
     this.currentLevel = this.currentDisplayLevel = this.incorrectGuesses = this.entryIndex = 0;
     this.showResetModal = this.showLossModal = this.showWinModal = false;
     this.guessNotAllowed = false;
@@ -210,18 +242,27 @@ export class GameComponent implements OnInit {
 
   setLetters(answer: string){
     this.letters = [];
-    this.enteredLetters = [];
+    if (!this.hasWon) this.enteredLetters = []; //don't reset this if the user has won so we can show the last answer
     this.absentLetters = []
     this.presentLetters = []
     this.correctLetters = []
 
     Array.from(answer).forEach(letter => {
       this.letters.push(letter)
-      this.enteredLetters.push({ //fill entered letter array with empty letters
-        letter: "",
-        state: "default"
-      })
+      if (!this.hasWon){
+        this.enteredLetters.push({ //fill entered letter array with empty letters
+          letter: "",
+          state: "default"
+        })
+      } else {
+        this.enteredLetters.push({
+          letter: letter,
+          state: "correct"
+        })
+      }
+
     })    
+
   }
 
   getNewPuzzle(){
@@ -229,6 +270,7 @@ export class GameComponent implements OnInit {
 
     setTimeout(function(){
       that.submissions = []
+      that.updateLocalStorage()
       that.setClue()
       that.entryIndex = 0;
       that.solved = false;
@@ -300,7 +342,6 @@ export class GameComponent implements OnInit {
         this.enteredLetters[i].state = "correct"
         correctLetterCount++
 
-
         //remove letter from temp array
         let index = tempLettersRemaining.indexOf(letter)
         if (index !== -1) tempLettersRemaining.splice(index, 1)
@@ -312,8 +353,9 @@ export class GameComponent implements OnInit {
     this.letters.forEach((letter, i) => {
 
       if (this.enteredLetters[i].state !== "correct"){
-        // console.log("checking " + letter) uncomment to console log answers
+
         if (this.letters.includes(this.enteredLetters[i].letter) && tempLettersRemaining.includes(this.enteredLetters[i].letter)){
+          
           this.enteredLetters[i].state = "present"
 
           this.presentLetters.push(this.enteredLetters[i].letter)
@@ -332,6 +374,7 @@ export class GameComponent implements OnInit {
     if (correctLetterCount === this.letters.length){
       this.guessNotAllowed = true
       this.currentLevel++
+      this.updateLocalStorage()
       this.entryIndex = -1 //to hide highlighter during confetti
       if (this.currentLevel === 7){
         this.handleWin()
@@ -345,6 +388,7 @@ export class GameComponent implements OnInit {
 
     } else {
       this.incorrectGuesses++
+      this.updateLocalStorage()
       this.shakeChecks = true;
       setTimeout(function(){
         that.shakeChecks = false;
@@ -355,6 +399,7 @@ export class GameComponent implements OnInit {
         this.handleLoss()
       } else {
         this.submissions.push(this.enteredLetters)
+        this.updateLocalStorage()
         let el = document.getElementById("letters-row-wrapper")
         this.enteredLetters = []
         this.letters.forEach(letter => {
@@ -373,15 +418,12 @@ export class GameComponent implements OnInit {
           })
         }, 1)
       }
-      
-
-
-
     }
-
   }
 
   handleLoss(){
+    this.hasLost = true;
+    this.updateLocalStorage()
     let that = this;
     let toastDuration = 2250
     this.toast(this.clue.answer, toastDuration)
@@ -392,6 +434,8 @@ export class GameComponent implements OnInit {
   }
 
   handleWin(){
+    this.hasWon = true;
+    this.updateLocalStorage()
     let that = this;
     this.renderWinConfetti()
     setTimeout(function(){ //wait until toast finishes to launch the modal
@@ -448,6 +492,52 @@ export class GameComponent implements OnInit {
   }
 
   /*------------------------------Helpers-------------------------------------*/
+
+  updateLocalStorage(){
+    localStorage.setItem('incorrectGuesses', "" + this.incorrectGuesses)
+    localStorage.setItem('currentDay', "" + this.daysSinceEpoch())
+    localStorage.setItem('currentLevel', "" + this.currentLevel)
+    localStorage.setItem('currentEntries', JSON.stringify(this.submissions))
+    localStorage.setItem('hasWon', "" + this.hasWon)
+    localStorage.setItem('hasLost', "" + this.hasLost)
+  }
+
+  resetLocalStorage(){
+    localStorage.removeItem('incorrectGuesses')
+    localStorage.removeItem('currentDay')
+    localStorage.removeItem('currentLevel')
+    localStorage.removeItem('currentEntries')
+    localStorage.removeItem('hasWon')
+    localStorage.removeItem('hasLost')
+  }
+
+  loadFromLocalStorage(){
+    let iG = localStorage.getItem('incorrectGuesses')
+    if (iG) this.incorrectGuesses = +iG
+
+    let cL = localStorage.getItem('currentLevel')
+    if (cL) {
+      this.currentLevel = +cL
+      this.currentDisplayLevel = this.currentLevel
+    }
+    
+    let cE = localStorage.getItem('currentEntries')
+    if (cE) this.submissions = JSON.parse(cE)
+
+    let hW = localStorage.getItem('hasWon')
+    if (hW) this.hasWon = hW === "true"
+
+    let hL = localStorage.getItem('hasLost')
+    if (hL) this.hasLost = hL === "true"
+      
+  }
+
+  isNewDay(){
+    let savedDay = localStorage.getItem('currentDay')
+    if (savedDay && +savedDay < this.daysSinceEpoch()) {
+      return true
+    } else return false
+  }
 
   isKeyPrintable(e: KeyboardEvent) {
     return allKeys.indexOf(e.key) === -1;
